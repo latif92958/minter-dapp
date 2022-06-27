e1a601245aff13051c861929cca3835c90c62b5a
latif92958 <onlyonlinemoney6@gmail.com>
2022-06-27 09:27:26 +0530

    changed contract constant file

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "0x1f2F1458A705770bC1f2a197bcDBDA9f2e3cB4F6"; //Replace with your own contract address
+const contractAddress = "0x93799F0c3Cf4E0Ca3CfF93590464cc94dAE31De8"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the AniMetaVerse NFT Project!!";
